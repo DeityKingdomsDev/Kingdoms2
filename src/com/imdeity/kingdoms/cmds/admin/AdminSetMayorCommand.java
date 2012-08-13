3c976f60519e931444bb6b1353c9a7227b19638d
Ryan Turner <ryan.e.t@gmail.com>
2012-08-13 00:35:36 -0500

    Updated /kingdomsadmin set-mayor command to set all other residents as not the mayor when appointing a new one

--- src/com/imdeity/kingdoms/cmds/admin/AdminSetMayorCommand.java
+++ src/com/imdeity/kingdoms/cmds/admin/AdminSetMayorCommand.java
@@ -1,5 +1,7 @@
 package com.imdeity.kingdoms.cmds.admin;
 
+import java.util.List;
+
 import org.bukkit.entity.Player;
 
 import com.imdeity.deityapi.api.DeityCommandReceiver;
@@ -31,6 +33,11 @@ public class AdminSetMayorCommand extends DeityCommandReceiver {
                 KingdomsMain.plugin.chat.out("The resident " + args[1] + " is invalid");
                 return true;
             }
+            List<String> townResidents = town.getResidentsNames();
+            for(String townResident : townResidents) { //Set all other residents to not be mayor.
+            	if(KingdomsManager.getResident(townResident).isMayor()) KingdomsManager.getResident(townResident).setMayor(false);
+            }
+            
             town.addResident(resident);
             town.setMayor(resident);
         }
