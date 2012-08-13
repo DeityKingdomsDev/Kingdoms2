96745c67a397878b4caf590eca111abe7bbbf607
Ryan Turner <ryan.e.t@gmail.com>
2012-08-13 03:13:58 -0500

    Fixed admin set-mayor command to not re-add player to residents list if already a resident. Copied code over from the console-side listener to the playerrun-side listener.

--- src/com/imdeity/kingdoms/cmds/admin/AdminSetMayorCommand.java
+++ src/com/imdeity/kingdoms/cmds/admin/AdminSetMayorCommand.java
@@ -2,6 +2,7 @@ package com.imdeity.kingdoms.cmds.admin;
 
 import java.util.List;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import com.imdeity.deityapi.api.DeityCommandReceiver;
@@ -37,8 +38,10 @@ public class AdminSetMayorCommand extends DeityCommandReceiver {
             for(String townResident : townResidents) { //Set all other residents to not be mayor.
             	if(KingdomsManager.getResident(townResident).isMayor()) KingdomsManager.getResident(townResident).setMayor(false);
             }
+            if(!townResidents.contains(resident.getName())) {
+            	town.addResident(resident);
+            }
             
-            town.addResident(resident);
             town.setMayor(resident);
         }
         KingdomsMain.plugin.chat.out("Set the mayor of " + args[0] + " to " + args[1]);
@@ -66,7 +69,13 @@ public class AdminSetMayorCommand extends DeityCommandReceiver {
                 KingdomsMain.plugin.chat.sendPlayerMessage(player, "The resident " + args[1] + " is invalid");
                 return true;
             }
-            town.addResident(resident);
+            List<String> townResidents = town.getResidentsNames();
+            for(String townResident : townResidents) { //Set all other residents to not be mayor.
+            	if(KingdomsManager.getResident(townResident).isMayor()) KingdomsManager.getResident(townResident).setMayor(false);
+            }
+            if(!townResidents.contains(resident.getName())) {
+            	town.addResident(resident);
+            }
             town.setMayor(resident);
         }
         KingdomsMain.plugin.chat.sendPlayerMessage(player, "Set the mayor of " + args[0] + " to " + args[1]);
